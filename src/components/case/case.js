4ca36e03114697e8bba931e82ca1b2fda92210d1
VictorPetr <victor.p@ss.rv.ua>
2019-12-23 17:34:42 +0200

    Change Homepage animation type

--- src/components/case/case.js
+++ src/components/case/case.js
@@ -11,14 +11,43 @@ const Case = props => {
 
     return (
         <div className={`cases-item cases-item--${itemClasses} cases-item--${props.index + 1}`}>
+            {/*<Controller>*/}
+            {/*    <Scene duration={2000}>*/}
+            {/*        <Tween*/}
+            {/*            from={{*/}
+            {/*                transform: "rotate3d(1, 0.49, -0.12, 45deg)"*/}
+            {/*            }}*/}
+            {/*            to={{*/}
+            {/*                transform: "rotate3d(1, 0.36, .04, -45deg)"*/}
+            {/*            }}*/}
+            {/*        >*/}
+            {/*            <div className="cases-item__thumb">*/}
+            {/*                <img className="cases-item__img" src={props.src} alt={props.title}/>*/}
+            {/*                {*/}
+            {/*                    props.thumbDesc*/}
+            {/*                        ? <span className="cases-item__description">{props.thumbDesc}</span>*/}
+            {/*                        : null*/}
+            {/*                }*/}
+            {/*            </div>*/}
+            {/*        </Tween>*/}
+            {/*    </Scene>*/}
+            {/*            <div className="cases-item__heading">*/}
+            {/*                <span className="case-heading-arrow"><Arrow /></span>*/}
+            {/*                <div className="case-heading-text">*/}
+            {/*                    <p>{props.category}</p>*/}
+            {/*                    <h3>{props.title}</h3>*/}
+            {/*                </div>*/}
+            {/*            </div>*/}
+            {/*</Controller>*/}
             <Controller>
                 <Scene duration={2000}>
                     <Tween
+                        wrapper={<div style={{overflow: 'hidden'}} />}
                         from={{
-                            transform: "rotate3d(1, 0.49, -0.12, 45deg)"
+                            transform: "scale3d(1, 1, 1)"
                         }}
                         to={{
-                            transform: "rotate3d(1, 0.36, .04, -45deg)"
+                            transform: "scale3d(1.3, 1.3, 1)"
                         }}
                     >
                         <div className="cases-item__thumb">
@@ -31,13 +60,13 @@ const Case = props => {
                         </div>
                     </Tween>
                 </Scene>
-                        <div className="cases-item__heading">
-                            <span className="case-heading-arrow"><Arrow /></span>
-                            <div className="case-heading-text">
-                                <p>{props.category}</p>
-                                <h3>{props.title}</h3>
-                            </div>
-                        </div>
+                <div className="cases-item__heading">
+                    <span className="case-heading-arrow"><Arrow /></span>
+                    <div className="case-heading-text">
+                        <p>{props.category}</p>
+                        <h3>{props.title}</h3>
+                    </div>
+                </div>
             </Controller>
         </div>
     )
